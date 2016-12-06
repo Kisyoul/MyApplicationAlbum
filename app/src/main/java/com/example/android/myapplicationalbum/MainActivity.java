267a4f1977779fde2d5ca5eddf2138d42df12725
kisyoul <kamisamax0@sina.com>
2016-12-06 21:26:45 +0800

    commit Firs night,I can not click the button

--- app/src/main/java/com/example/android/myapplicationalbum/MainActivity.java
+++ app/src/main/java/com/example/android/myapplicationalbum/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.android.myapplicationalbum;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void OpenHotMovie(){
+        Toast.makeText(this,R.string.Hotmovie,Toast.LENGTH_SHORT).show();
+    }
+    public void OpenEagle(){
+        Toast.makeText(this,R.string.eagle,Toast.LENGTH_SHORT).show();
+    }
+    public void OpenReader(){
+        Toast.makeText(this,R.string.reader,Toast.LENGTH_SHORT).show();
+    }
+    public void OpenNews(){
+        Toast.makeText(this,R.string.news,Toast.LENGTH_SHORT).show();
+    }
+    public void OpenDesign(){
+        Toast.makeText(this,R.string.design,Toast.LENGTH_SHORT).show();
     }
 }
